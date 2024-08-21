b5891ba5433dff992b7dccf1eee9a0c89ee23abd
KashifKhn <kashifkhnx04@gmail.com>
2024-08-21 10:29:57 +0500

    refactor: Update  layout component to modify as shad cn doc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Toxi Scan",
-  description: "ToxiScan is a full-stack web application designed to analyze, manage, and improve the quality of online comments and text",
+  description:
+    "ToxiScan is a full-stack web application designed to analyze, manage, and improve the quality of online comments and text",
 };
 
 export default function RootLayout({
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}>
+        {children}
+      </body>
     </html>
   );
 }
