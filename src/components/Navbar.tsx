267b08165121df7f612c3f6c1fe12fe547a2dbd4
KashifKhn <kashifkhnx04@gmail.com>
2024-08-21 11:42:16 +0500

    refactor: Update Navbar component to include ThemeToggle for theme toggling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import NavLink from "./NavLink";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+    <nav className="bg- text-primary flex justify-center px-4 shadow-sm op shadow-primary">
       <NavLink href="/">Home</NavLink>
       <NavLink href="/text">Text</NavLink>
       <NavLink href="/upload">Upload</NavLink>
       <NavLink href="/comment-">Comment</NavLink>
+      <div className="ml-auto text-primary my-auto pt-1">
+        <ThemeToggle />
+      </div>
     </nav>
   );
 };
