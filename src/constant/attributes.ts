3762a1a638910f4ee63250d3fb170123705cf887
KashifKhn <kashifkhnx04@gmail.com>
2024-08-26 00:38:06 +0500

    feat: Add attributes constant file with toxicity attributes

---
+++ src/constant/attributes.ts
@@ -0,0 +1,35 @@
+export type Attribute = {
+  attrName: string;
+  description: string;
+};
+
+export const attributes: Attribute[] = [
+  {
+    attrName: "TOXICITY",
+    description:
+      "A rude, disrespectful, or unreasonable comment that is likely to make people leave a discussion.",
+  },
+  {
+    attrName: "SEVERE_TOXICITY",
+    description:
+      "A very hateful, aggressive, disrespectful comment or otherwise very likely to make people leave a discussion.",
+  },
+  {
+    attrName: "IDENTITY_ATTACK",
+    description:
+      "A personal attack on someone, including explicit or implicit threats.",
+  },
+  {
+    attrName: "INSULT",
+    description:
+      "A rude or disrespectful comment that is likely to make people leave a discussion.",
+  },
+  {
+    attrName: "PROFANITY",
+    description: "Obscene language, including swearing.",
+  },
+  {
+    attrName: "THREAT",
+    description: "A threat to someone's safety or well-being.",
+  },
+] as const;
