bef3bd59518f1edde4e45e08e08c40b74eea3ca8
KashifKhn <kashifkhnx04@gmail.com>
2024-08-26 00:43:07 +0500

    feat: Update NavLink href in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg- text-primary flex justify-center px-4 shadow-sm op shadow-primary">
       <NavLink href="/">Home</NavLink>
-      <NavLink href="/text">Text</NavLink>
+      <NavLink href="/text-analysis">Text</NavLink>
       <NavLink href="/upload">Upload</NavLink>
       <NavLink href="/comment-">Comment</NavLink>
       <div className="ml-auto text-primary my-auto pt-1">
