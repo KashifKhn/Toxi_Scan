1553c1428a5f0b7ccc4e60bd268138886609dfa8
KashifKhn <kashifkhnx04@gmail.com>
2024-08-27 18:25:48 +0500

    fix: formdata schema to use enum array and ensure the redue function use the type safe

--- src/app/_actions/textAnalysis.ts
+++ src/app/_actions/textAnalysis.ts
@@ -1,14 +1,21 @@
 "use server";
+import { attributeNames } from "@/constant/attributes";
 import axios from "axios";
 import { z } from "zod";
+
+type ValidAttribute = (typeof attributeNames)[number];
+
+type RequestedAttributes = Record<ValidAttribute, { scoreThreshold: number }>;
 
 const API_KEY = process.env.PERSPECTIVE_API_KEY as string;
 const PERSPECTIVE_API_URL =
   "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze";
 
+const attrSchema = z.array(z.enum(attributeNames));
+
 const analyzeSchema = z.object({
   text: z.string().min(1, "Text is required"),
-  attr: z.array(z.string()).nonempty("At least one attribute must be selected"),
+  attr: attrSchema,
   threshold: z.preprocess(
     (value) => parseFloat(value as string),
     z.number().min(0).max(100),
@@ -30,13 +37,14 @@ export const analyze = async (_prevState: unknown, formData: FormData) => {
 
   const { attr, text, threshold } = result.data;
 
-  const requestedAttributes = attr.reduce((acc, attr) => {
-    acc[attr] = { scoreThreshold: threshold / 100 };
-    return acc;
-  }, {});
-
-  console.log("text", text);
-  console.log("requestedAttributes", requestedAttributes);
+  const requestedAttributes: RequestedAttributes =
+    attr.reduce<RequestedAttributes>(
+      (acc: RequestedAttributes, attribute: ValidAttribute) => {
+        acc[attribute] = { scoreThreshold: threshold / 100 };
+        return acc;
+      },
+      {} as RequestedAttributes,
+    );
 
   try {
     const response = await axios.post(`${PERSPECTIVE_API_URL}?key=${API_KEY}`, {
