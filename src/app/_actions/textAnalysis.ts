4309ef5d6464bea76e6630beb63bba958b653ba4
KashifKhn <kashifkhnx04@gmail.com>
2024-08-27 14:05:10 +0500

    fix: the api to use dynamic attribute from form data

--- src/app/_actions/textAnalysis.ts
+++ src/app/_actions/textAnalysis.ts
@@ -1,30 +1,27 @@
 "use server";
-
 import axios from "axios";
 
 const API_KEY = process.env.PERSPECTIVE_API_KEY as string;
 const PERSPECTIVE_API_URL =
   "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze";
 
-export const analyze = async (formData: FormData) => {
+export const analyze = async (_prevState: unknown, formData: FormData) => {
+  const selectedAttr = formData.getAll("attr");
   const data = Object.fromEntries(formData.entries());
 
-  console.log("data", data);
+  const requestedAttributes = selectedAttr.reduce((acc, attr) => {
+    acc[attr] = {};
+    return acc;
+  }, {});
 
   try {
     const response = await axios.post(`${PERSPECTIVE_API_URL}?key=${API_KEY}`, {
       comment: { text: data.text },
       languages: ["en"],
-      requestedAttributes: {
-        TOXICITY: {},
-        SEVERE_TOXICITY: {},
-        IDENTITY_ATTACK: {},
-        INSULT: {},
-        PROFANITY: {},
-        THREAT: {},
-      },
+      requestedAttributes: requestedAttributes,
+      theshold: data.threshold,
     });
-    console.log(response.data);
+    console.log("hello", response.data);
   } catch (error) {
     console.error("error" + error);
   }
