d34a058c889087a675162bdb04087f73d0bd860b
KashifKhn <kashifkhnx04@gmail.com>
2024-08-28 21:44:05 +0500

    refactor: adding the upstash redis server

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_SECRET_KEY as string);
 
 export const saveToRedis = async (key: string, value: any) => {
   await redis.set(key, JSON.stringify(value), "EX", 3600);
